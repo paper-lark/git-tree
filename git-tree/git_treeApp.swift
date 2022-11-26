1847bf291f7cbbf6f744d54c6a8650b560e5965b
Max Zhuravsky <paperlark@yandex.com>
2022-11-26 16:14:35 +0300

    Initial Commit

---
+++ git-tree/git_treeApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_treeApp.swift
+//  git-tree
+//
+//  Created by Max Zhuravsky on 26.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct git_treeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
